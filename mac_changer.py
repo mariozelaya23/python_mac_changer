ccc58e05f6df648be20081b2e7d805b00e05f849
Mario Zelaya <mariozelaya23@gmail.com>
2022-03-17 19:44:37 -0500

    changing subprocess function to a list instead of concatenation

--- mac_changer.py
+++ mac_changer.py
@@ -5,6 +5,7 @@ new_mac = input("New mac > ")
 
 print("[+] Changing MAC address for " + interface + " to " + new_mac)
 
-subprocess.call("ifconfig " + interface + " down", shell=True)
-subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
-subprocess.call("ifconfig " + interface + " up", shell=True)
+subprocess.call(["ifconfig", interface, "down"])
+subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+subprocess.call(["ifconfig", interface, "up"])
+
