77ad40f094b3f32f15bbe7c0ec7c0d23ec324035
Mario Zelaya <mariozelaya23@gmail.com>
2022-03-17 19:59:12 -0500

    adding optparse module

--- mac_changer.py
+++ mac_changer.py
@@ -1,4 +1,14 @@
+#!/usr/bin/env python3
+
 import subprocess
+import optparse
+# optparse get the arguments from user and parse them to our code
+
+parser = optparse.OptionParser()
+
+parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+
+parser.parse_args()
 
 interface = input("Enter the interface > ")
 new_mac = input("New mac > ")
