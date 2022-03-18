5b178d9fdd07e50c1a2efc46555ba8fbe5b2b8b4
Mario Zelaya <mariozelaya23@gmail.com>
2022-03-18 13:31:59 -0500

    adding if statement to show to user a messge if user not write the interface and mac

--- mac_changer.py
+++ mac_changer.py
@@ -9,7 +9,12 @@ def get_argument():
     parser = optparse.OptionParser()
     parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
     parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
-    return parser.parse_args()
+    (options, arguments) = parser.parse_args()
+    if not options.interface:
+        parser.error("[-] Please specify an interface, use --help for more info.")
+    if not options.new_mac:
+        parser.error("[-] Please specify an new mac, use --help for more info.")
+    return options
 
 
 def change_mac(interface, new_mac):
@@ -19,7 +24,7 @@ def change_mac(interface, new_mac):
     subprocess.call(["ifconfig", interface, "up"])
 
 
-(options, arguments) = get_argument()
+options = get_argument()
 
 change_mac(options.interface, options.new_mac)
 
