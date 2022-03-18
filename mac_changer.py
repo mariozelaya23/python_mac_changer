5aedd655781b02bfda0ff5b022201ac819391df0
Mario Zelaya <mariozelaya23@gmail.com>
2022-03-18 12:59:00 -0500

    adding function change_mac and removing variables

--- mac_changer.py
+++ mac_changer.py
@@ -4,6 +4,14 @@ import subprocess
 import optparse
 # optparse get the arguments from user and parse them to our code
 
+
+def change_mac(interface, new_mac):
+    print("[+] Changing MAC address for " + interface + " to " + new_mac)
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+
+
 parser = optparse.OptionParser()
 
 parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
@@ -11,12 +19,6 @@ parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
 
 (options, arguments) = parser.parse_args()
 
-interface = options.interface
-new_mac = options.new_mac
+change_mac(options.interface, options.new_mac)
 
-print("[+] Changing MAC address for " + interface + " to " + new_mac)
-
-subprocess.call(["ifconfig", interface, "down"])
-subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
-subprocess.call(["ifconfig", interface, "up"])
 
